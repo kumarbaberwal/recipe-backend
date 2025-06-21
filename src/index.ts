8e0ca3fa7e22e8baaaa91062fff5fe5e481c3e48
kumarbaberwal <niteshkumarghn1@gmail.com>
2025-06-21 21:11:46 +0530

    .env.example added

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { ENV } from './configs/env';
 
 
 const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json({
+        message: 'Kumar',
+    })
+})
+
 const PORT = ENV.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on: http://localhost:${PORT}`);
